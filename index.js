c5542cee39fb916fea46a7c316b34dc4a068879e
lulamile99 <lulabenni45@gmail.com>
2024-05-15 19:39:20 +0200

    looking up in an array of objects

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ console.log(my_name);
 //console.log(my_name);*/
 
 //objects
-let person = {
+/*let person = {
     "name": "Denver Vlohovic",
     "Country": "Croatia",
     "Age": 31,
@@ -27,4 +27,43 @@ console.log(person)
 delete person.Country //Deleting a property in objects
 console.log(person)
 person.Continent = "Europe"; //Adding a property in objects
-console.log(person)
+console.log(person)*/
+
+//Loops
+let profile = [
+    {
+        "firstName": "vini",
+        "surname": "jr",
+        "number": "2858735235",
+        "likes": ["Fifa","5 vs 1","football","Liverpool"]
+    },
+    {
+        "firstName": "Arda",
+        "surname": "Guller",
+        "number": "4567346788",
+        "likes": ["Fifa","braai","football"]
+    },
+    {
+        "firstName": "Roydrigo",
+        "surname": "Goes",
+        "number": "98876845738",
+        "likes": ["beach","LW","Man City"]
+    },
+    {
+        "firstName": "Jude",
+        "surname": "Bellingham",
+        "number": "545735783564",
+        "likes": ["training","10","Barcelona"]
+    },
+];
+
+function lookUpProfile(name,prop){
+    for(let i = 0; i<profile.length;i++ ){
+        if(profile[i].firstName===name){
+            if(profile[i].hasOwnProperty(prop)){
+                return prop+": "+profile[i][prop];
+            }
+        }
+    }
+}
+console.log(lookUpProfile("vini","likes"))
