180078a2b5d897762274f094830bfb2ca49f189d
lulamile99 <lulabenni45@gmail.com>
2024-05-15 18:34:30 +0200

    Objects

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-var my_name = "Benni Plati";
+/*var my_name = "Benni Plati";
 function myName(){
     let my_name = "Lulamile Plati";
     return my_name;
 }
 console.log(myName());
 console.log(my_name);
-//console.log(my_name);
+//console.log(my_name);*/
+
+//objects
+let person = {
+    "name": "Denver Vlohovic",
+    "Country": "Croatia",
+    "Age": 31,
+    "hobbies": ["football","Tennis","Golf"]
+};
+console.log(person)
+console.log("Name: "+person.name);
+console.log("Number Of Hobbies: "+person.hobbies.length)
+person.hobbies.push("Video Games");
+console.log("Number Of Hobbies: "+person.hobbies.length)
+person.hobbies.pop();
+console.log(person)
+person.hobbies.shift();
+console.log(person)
